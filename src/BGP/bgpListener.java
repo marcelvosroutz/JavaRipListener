95661f7235b2dd64f59aca1cc73af6ca41e3dd21
Marcel Vos <marcel.vos@routz.nl>
2019-11-14 22:09:35 +0100

    -- added simple while (alive) to spawn new socket when old one is closed by peer

--- src/BGP/bgpListener.java
+++ src/BGP/bgpListener.java
@@ -31,68 +31,71 @@ public class bgpListener extends Thread  {
 
     public void run() {
 
-        try {
-            // show start message
-            System.out.println("Tread: Waiting for peering request on TCP/179 (BGP) traffic");
+        // if Session gets closed.. start a new one instantly to listen for a new session
+        while (true) {
+            try {
+                // show start message
+                System.out.println("Tread: Waiting for peering request on TCP/179 (BGP) traffic");
 
-            // construct our socket
-            conn = new ServerSocket(179);
-            recvSock = conn.accept();
-            is = new DataInputStream(recvSock.getInputStream());
+                // construct our socket
+                conn = new ServerSocket(179);
+                recvSock = conn.accept();
+                is = new DataInputStream(recvSock.getInputStream());
 
-            // bgp header is 19 bytes
-            byte[] bgpHeader = new byte[19];
-            byte[] bgpPayload;
+                // bgp header is 19 bytes
+                byte[] bgpHeader = new byte[19];
+                byte[] bgpPayload;
 
-            while (conn != null ) {
-                // reads the first 19 bytes into BGP header
-                is.readFully(bgpHeader);
+                while (conn != null) {
+                    // reads the first 19 bytes into BGP header
+                    is.readFully(bgpHeader);
 
-                // parse the BGP header
-                System.out.println("BGP HEADER: " + byteArrayToHex(bgpHeader));
-                byte[] bgpMarker = new byte[16];
-                byte[] bgpLength = new byte[2];
-                byte[] bgpType = new byte[1];
+                    // parse the BGP header
+                    System.out.println("BGP HEADER: " + byteArrayToHex(bgpHeader));
+                    byte[] bgpMarker = new byte[16];
+                    byte[] bgpLength = new byte[2];
+                    byte[] bgpType = new byte[1];
 
-                // copy bytes from bgpHeader into marker, length and type
-                System.arraycopy(bgpHeader, 0, bgpMarker, 0, 16);
-                System.arraycopy(bgpHeader, 16, bgpLength, 0, 2);
-                System.arraycopy(bgpHeader, 18, bgpType, 0, 1);
+                    // copy bytes from bgpHeader into marker, length and type
+                    System.arraycopy(bgpHeader, 0, bgpMarker, 0, 16);
+                    System.arraycopy(bgpHeader, 16, bgpLength, 0, 2);
+                    System.arraycopy(bgpHeader, 18, bgpType, 0, 1);
 
-                // determine payLoad size (length field - header size)
-                int messageSize = (bgpLength[0] & 0xFF) + (bgpLength[1] & 0xFF)-19;
+                    // determine payLoad size (length field - header size)
+                    int messageSize = (bgpLength[0] & 0xFF) + (bgpLength[1] & 0xFF) - 19;
 
-                bgpPayload = new byte[messageSize];
+                    bgpPayload = new byte[messageSize];
 
-                // read the BGP payload
-                is.readFully(bgpPayload);
+                    // read the BGP payload
+                    is.readFully(bgpPayload);
 
-                switch (bgpType[0]) {
-                    case BGP_OPEN:
-                        System.out.println("Received BGP_OPEN");
-                        System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
-                        break;
-                    case BGP_UPDATE:
-                        System.out.println("Received BGP_UPDATE");
-                        System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
-                        break;
-                    case BGP_NOTIFICATION:
-                        System.out.println("Received BGP_NOTIFICATION");
-                        System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
-                        break;
-                    case BGP_KEEPALIVE:
-                        System.out.println("Received BGP_KEEPALIVE");
-                        System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
-                        break;
-                    default:
-                        System.out.print("Received UNKNOWN bgp type: " + byteArrayToHex(bgpType));
-                        System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
-                        break;
+                    switch (bgpType[0]) {
+                        case BGP_OPEN:
+                            System.out.println("Received BGP_OPEN");
+                            System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
+                            break;
+                        case BGP_UPDATE:
+                            System.out.println("Received BGP_UPDATE");
+                            System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
+                            break;
+                        case BGP_NOTIFICATION:
+                            System.out.println("Received BGP_NOTIFICATION");
+                            System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
+                            break;
+                        case BGP_KEEPALIVE:
+                            System.out.println("Received BGP_KEEPALIVE");
+                            System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
+                            break;
+                        default:
+                            System.out.print("Received UNKNOWN bgp type: " + byteArrayToHex(bgpType));
+                            System.out.println("PAYLOAD: " + byteArrayToHex(bgpPayload));
+                            break;
+                    }
                 }
-            }
 
-        } catch (IOException errorMessage) {
-            System.out.println("Unable to listen on socket: " + errorMessage);
+            } catch (IOException errorMessage) {
+                System.out.println("TCP Session closed");
+            }
         }
 
     }
